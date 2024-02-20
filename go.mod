e364d5dd3769ffd3ac69f70cf6ad24315d12f566
David Rio Deiros <driodeiros@gmail.com>
2024-02-20 11:33:35 -0500

    wip

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/prometheus/client_golang v1.18.0
+	golang.org/x/oauth2 v0.17.0
 	tailscale.com v1.60.0
 )
 
@@ -35,6 +36,7 @@ require (
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.1-0.20230522191255-76236955d466 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/nftables v0.1.1-0.20230115205135-9aa6fdf5a28c // indirect
@@ -75,18 +77,19 @@ require (
 	github.com/x448/float16 v0.8.4 // indirect
 	go4.org/mem v0.0.0-20220726221520-4f986261bf13 // indirect
 	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
-	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
 	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
 	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
 	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 	gvisor.dev/gvisor v0.0.0-20240119233241-c9c1d4f9b186 // indirect
 	inet.af/peercred v0.0.0-20210906144145-0893ea02156a // indirect
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
+	"golang.org/x/oauth2/clientcredentials"
 	"tailscale.com/tsnet"
 )
 
@@ -19,6 +23,23 @@ var (
 
 func main() {
 	flag.Parse()
+
+	// You need an API access token with network-logs:read
+	clientId := os.Getenv("OAUTH_CLIENT_ID")
+	if clientId == "" {
+		log.Fatal("Please, provide a OAUTH_CLIENT_ID option")
+	}
+	clientSecret := os.Getenv("OAUTH_CLIENT_SECRET")
+	if clientSecret == "" {
+		log.Fatal("Please, provide a OAUTH_CLIENT_SECRET option")
+	}
+	tailnetName := os.Getenv("TAILNET_NAME")
+	if tailnetName == "" {
+		log.Fatal("Please, provide a TAILNET_NAME option")
+	}
+
+	apiUrl := fmt.Sprintf("https://api.tailscale.com/api/v2/tailnet/%s/devices", *tailnetName)
+	_ = genAPIAccessToken(apiUrl)
 
 	s := new(tsnet.Server)
 	s.Hostname = *hostname
@@ -36,17 +57,33 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var aGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "drio_random",
-			Help: "A drio random gauge",
-		},
-		[]string{"method"},
-	)
-	aGauge.WithLabelValues("foo").Set(123)
-	prometheus.MustRegister(aGauge)
-
+	createMetric()
 	http.Handle("/metrics", promhttp.Handler())
+
+	// TODO
+	// - [] Make a request to the API to make sure it works (https://github.com/tailscale/tailscale/blob/main/api.md#list-tailnet-devices)
+	//      Store data in DS1
+	// - [] Write a client that makes a request to the API to get net logs
+	//      Put the data in the DS2
+	// - [] Update the metrics using DS1 and DS2 (lock)
+	// - [] serve metrics via prometheus
+	//
+	// Go Routings
+	// 1. get api data
+	// 2. get log data
+	// 3. update metrics
+	//
+	// Metrics:
+	// ts_bytes_send_per_sec_counter{hostname, proto, dst, type}
+	// 3 more...
+	//
+	// ts_number_hosts_gauge{os="", external=""} = num
+	// ts_client_updates_gauge{hostname=""} = 0 1
+	// ts_latencies_gauge{hostname, derp_server} = num
+	// ts_tags_gauge{hostname} = num tags
+	// ts_udp_ok_gauge{hostname} = 0 or 1
+	// ts_versions{version=""} = num hosts
+	// ts_client_needs_updates{hostname=""} = 0 1
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		who, err := lc.WhoIs(r.Context(), r.RemoteAddr)
@@ -57,4 +94,37 @@ func main() {
 	})
 	log.Printf("starting server on %s", *addr)
 	log.Fatal(http.Serve(ln, nil))
+}
+
+func createMetric() {
+	var aGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "drio_random",
+			Help: "A drio random gauge",
+		},
+		[]string{"method"},
+	)
+	aGauge.WithLabelValues("foo").Set(123)
+	prometheus.MustRegister(aGauge)
+}
+
+func genAPIAccessToken(apiUrl string) string {
+	var oauthConfig = &clientcredentials.Config{
+		ClientID:     os.Getenv("OAUTH_CLIENT_ID"),
+		ClientSecret: os.Getenv("OAUTH_CLIENT_SECRET"),
+		TokenURL:     "https://api.tails cale.com/api/v2/oauth/token",
+	}
+
+	client := oauthConfig.Client(context.Background())
+	resp, err := client.Get(apiUrl)
+	if err != nil {
+		log.Fatalf("error getting keys: %v", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("error reading response body: %v", err)
+	}
+
+	return string(body)
 }
