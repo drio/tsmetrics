7d9a40505dfe0828b56e8a5037bf1483321e4291
David Rio Deiros <driodeiros@gmail.com>
2024-02-28 11:27:50 -0500

    wip

--- api-logs_test.go
+++ api-logs_test.go
@@ -93,88 +93,99 @@ func TestMain(m *testing.M) {
 }
 
 func TestAPIMetrics(t *testing.T) {
-	t.Run("metric tailscale_hosts", func(t *testing.T) {
-		app.LMData.Init()
+	app.LMData.Init()
 
-		faClient.SetDevices(jsonDevices)
-		app.updateAPIMetrics(&faClient)
+	faClient.SetDevices(jsonDevices)
+	app.updateAPIMetrics(&faClient)
 
-		mName := "tailscale_hosts"
-		c := qt.New(t)
-		hostToMetric := gatherLabels("hostname", mName, t)
-		c.Assert(len(hostToMetric), qt.Equals, 2)
+	mName := "tailscale_hosts"
+	c := qt.New(t)
+	hostToMetric := gatherLabels("hostname", mName, t)
+	c.Assert(len(hostToMetric), qt.Equals, 2)
 
-		// TODO: Pull this from the json truth
-		hello := hostToMetric["hello"]
-		c.Assert(len(hello), qt.Equals, 6)
-		c.Assert(hello["hostname"], qt.Equals, "hello")
-		c.Assert(hello["update_available"], qt.Equals, "false")
-		c.Assert(hello["os"], qt.Equals, "linux")
-		c.Assert(hello["is_external"], qt.Equals, "true")
-		c.Assert(hello["user"], qt.Equals, "perucho@foo.net")
-		c.Assert(hello["client_version"], qt.Equals, "1.1.1")
+	// TODO: Pull this from the json truth
+	hello := hostToMetric["hello"]
+	c.Assert(len(hello), qt.Equals, 6)
+	c.Assert(hello["hostname"], qt.Equals, "hello")
+	c.Assert(hello["update_available"], qt.Equals, "false")
+	c.Assert(hello["os"], qt.Equals, "linux")
+	c.Assert(hello["is_external"], qt.Equals, "true")
+	c.Assert(hello["user"], qt.Equals, "perucho@foo.net")
+	c.Assert(hello["client_version"], qt.Equals, "1.1.1")
 
-		foo := hostToMetric["foo"]
-		c.Assert(len(foo), qt.Equals, 6)
-		c.Assert(foo["hostname"], qt.Equals, "foo")
-		c.Assert(foo["update_available"], qt.Equals, "true")
-		c.Assert(foo["os"], qt.Equals, "macos")
-		c.Assert(foo["is_external"], qt.Equals, "false")
-		c.Assert(foo["user"], qt.Equals, "rufus@foo.net")
-		c.Assert(foo["client_version"], qt.Equals, "2.2.2")
-
-	})
+	foo := hostToMetric["foo"]
+	c.Assert(len(foo), qt.Equals, 6)
+	c.Assert(foo["hostname"], qt.Equals, "foo")
+	c.Assert(foo["update_available"], qt.Equals, "true")
+	c.Assert(foo["os"], qt.Equals, "macos")
+	c.Assert(foo["is_external"], qt.Equals, "false")
+	c.Assert(foo["user"], qt.Equals, "rufus@foo.net")
+	c.Assert(foo["client_version"], qt.Equals, "2.2.2")
 }
 
-func TestLogMetrics(t *testing.T) {
-	t.Run("tailscale_tx_packets", func(t *testing.T) {
-		app.LMData.Init()
+func TestTxPackets(t *testing.T) {
+	app.LMData.Init()
 
-		flClient.SetJson(logOne)
-		app.getNewLogData(&flClient)
-		app.consumeNewLogData()
+	flClient.SetJson(logOne)
+	app.getNewLogData(&flClient)
+	app.consumeNewLogData()
 
-		mName := "tailscale_tx_packets"
-		c := qt.New(t)
-		srcToMetric := gatherLabels("src", mName, t)
-		c.Assert(len(srcToMetric), qt.Equals, 3)
+	mName := "tailscale_tx_packets"
+	c := qt.New(t)
+	srcToMetric := gatherLabels("src", mName, t)
+	c.Assert(len(srcToMetric), qt.Equals, 3)
 
-		src := "100.111.22.33"
-		val, found := getMetricValueWithSrc(src, mName, t)
-		fmt.Printf("\n%f, %t\n", val, found)
-		c.Assert(found, qt.Equals, true)
-		c.Assert(val, qt.Equals, 40.0)
+	src := "100.111.22.33"
+	val, found := getMetricValueWithSrc(src, mName, t)
+	fmt.Printf("\n%f, %t\n", val, found)
+	c.Assert(found, qt.Equals, true)
+	c.Assert(val, qt.Equals, 40.0)
 
-		// Make a new call to get new counters and check again the metric values
-		// the second log file matches the first one so the values should just double.
-		flClient.SetJson(logTwo)
-		app.getNewLogData(&flClient)
-		app.consumeNewLogData()
-		val, found = getMetricValueWithSrc(src, mName, t)
-		fmt.Printf("\n%f, %t\n", val, found)
-		c.Assert(found, qt.Equals, true)
-		c.Assert(val, qt.Equals, 80.0)
-	})
+	mName = "tailscale_rx_packets"
+	val, found = getMetricValueWithSrc(src, mName, t)
+	c.Assert(found, qt.Equals, true)
+	c.Assert(val, qt.Equals, 22.0)
 
-	t.Run("We resolve names", func(t *testing.T) {
-		c := qt.New(t)
-		app.LMData.Init()
+	mName = "tailscale_tx_bytes"
+	val, found = getMetricValueWithSrc(src, mName, t)
+	c.Assert(found, qt.Equals, true)
+	c.Assert(val, qt.Equals, 3.0)
 
-		flClient.SetJson(jsonDevicesTwo)
-		tailNet := "dummy"
-		app.NamesByAddr = mustMakeNamesByAddr(&tailNet, &flClient)
+	mName = "tailscale_rx_bytes"
+	val, found = getMetricValueWithSrc(src, mName, t)
+	c.Assert(found, qt.Equals, true)
+	c.Assert(val, qt.Equals, 60.0)
 
-		flClient.SetJson(logThree)
-		app.getNewLogData(&flClient)
-		app.consumeNewLogData()
+	// Make a new call to get new counters and check again the metric values
+	// the second log file matches the first one so the values should just double.
+	mName = "tailscale_tx_packets"
+	flClient.SetJson(logTwo)
+	app.getNewLogData(&flClient)
+	app.consumeNewLogData()
+	val, found = getMetricValueWithSrc(src, mName, t)
+	fmt.Printf("\n%f, %t\n", val, found)
+	c.Assert(found, qt.Equals, true)
+	c.Assert(val, qt.Equals, 80.0)
+}
 
-		mName := "tailscale_tx_packets"
-		src := "hello"
-		val, found := getMetricValueWithSrc(src, mName, t)
-		fmt.Printf("\n%f, %t\n", val, found)
-		c.Assert(found, qt.Equals, true)
-		c.Assert(val, qt.Equals, 130.0)
-	})
+func TestResolveNames(t *testing.T) {
+	c := qt.New(t)
+	app.LMData.Init()
+
+	flClient.SetJson(jsonDevicesTwo)
+	tailNet := "dummy"
+	app.NamesByAddr = mustMakeNamesByAddr(&tailNet, &flClient)
+
+	flClient.SetJson(logThree)
+	app.getNewLogData(&flClient)
+	app.consumeNewLogData()
+
+	mName := "tailscale_tx_packets"
+	src := "hello"
+	val, found := getMetricValueWithSrc(src, mName, t)
+	fmt.Printf("\n%f, %t\n", val, found)
+	c.Assert(found, qt.Equals, true)
+	c.Assert(val, qt.Equals, 130.0)
 }
 
 func getMetricValueWithSrc(src, mName string, t *testing.T) (float64, bool) {
